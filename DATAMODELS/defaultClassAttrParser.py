0a433a577b7c5ae6f073403c29c547a2d8b41ed8
Leo Paggen <lpaggen@gmail.com>
2024-07-25 16:43:30 +0200

    fixed minor bug in attribute defaults

--- DATAMODELS/defaultClassAttrParser.py
+++ DATAMODELS/defaultClassAttrParser.py
@@ -2,7 +2,7 @@ import os
 import json
 import pprint
 
-PATH_TO_JSON_DATA = ""
+PATH_TO_JSON_DATA = "C:/Users/lpagg/Documents/research_cisco/ACIBILIZER/DATAMODELS/fvClasses/"
 
 # this script allows to get a dictionary containing all default attribute values for classes
 # its use is to determine which attributes to delete entirely when rebuilding yml playbooks
@@ -26,24 +26,21 @@ def parse_attr_defaults(dir):
         entry_dict = {} # serves as nested dict in "out"
         file_path = os.path.join(dir, file)
         with open(file_path, 'r') as file_:
-            try:
-                data = json.load(file_)
-            except(json.decoder.JSONDecodeError):
-                break
+            data = json.load(file_)
             # will simply need to do a switch to address other prefixes, it is the same logic
-            className = "fv:" + file.split(".")[0][2:] # gets the fv:xxxx format in the json
-            print(className)
-            try:
-                for key, value in data[className]['properties'].items():
-                    try:
-                        result = data[className]['properties'][key]["default"]
-                        if data[className]['properties'][key]["default"] in yml_bool_list: # yml -> "true" == "yes", etc
-                            result = yml_bool_map[data[className]['properties'][key]["default"]]
-                        entry_dict[key] = result # sets value to default
-                    except(KeyError):
-                        entry_dict[key] = ""
-            except(KeyError):
-                pass
+            if file.split(".")[0][:2] == "fv":
+                className = "fv:" + file.split(".")[0][2:]
+                print(className)
+            else:
+                className = "fv:" + file.split(".")[0] # gets the fv:xxxx format in the json
+            for key, value in data[className]['properties'].items():
+                try:
+                    result = data[className]['properties'][key]["default"]
+                    if data[className]['properties'][key]["default"] in yml_bool_list: # yml -> "true" == "yes", etc
+                        result = yml_bool_map[data[className]['properties'][key]["default"]]
+                    entry_dict[key] = result # sets value to default
+                except(KeyError):
+                    entry_dict[key] = ""
 
         out[className.replace(":", "")] = entry_dict
         
