f8ba18797230e1a6ff9f3a179e3672881605739c
Leo Paggen <lpaggen@gmail.com>
2024-07-25 16:12:43 +0200

    fixed bug in parser, updated defaults

--- DATAMODELS/defaultClassAttrParser.py
+++ DATAMODELS/defaultClassAttrParser.py
@@ -26,17 +26,24 @@ def parse_attr_defaults(dir):
         entry_dict = {} # serves as nested dict in "out"
         file_path = os.path.join(dir, file)
         with open(file_path, 'r') as file_:
-            data = json.load(file_)
+            try:
+                data = json.load(file_)
+            except(json.decoder.JSONDecodeError):
+                break
             # will simply need to do a switch to address other prefixes, it is the same logic
-            className = "fv:" + file.split(".")[0] # gets the fv:xxxx format in the json
-            for key, value in data[className]['properties'].items():
-                try:
-                    result = data[className]['properties'][key]["default"]
-                    if data[className]['properties'][key]["default"] in yml_bool_list: # yml -> "true" == "yes", etc
-                        result = yml_bool_map[data[className]['properties'][key]["default"]]
-                    entry_dict[key] = result # sets value to default
-                except(KeyError):
-                    entry_dict[key] = None
+            className = "fv:" + file.split(".")[0][2:] # gets the fv:xxxx format in the json
+            print(className)
+            try:
+                for key, value in data[className]['properties'].items():
+                    try:
+                        result = data[className]['properties'][key]["default"]
+                        if data[className]['properties'][key]["default"] in yml_bool_list: # yml -> "true" == "yes", etc
+                            result = yml_bool_map[data[className]['properties'][key]["default"]]
+                        entry_dict[key] = result # sets value to default
+                    except(KeyError):
+                        entry_dict[key] = ""
+            except(KeyError):
+                pass
 
         out[className.replace(":", "")] = entry_dict
         
