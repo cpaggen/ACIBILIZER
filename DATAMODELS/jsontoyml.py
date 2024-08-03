1f9ea5d9426ee9a785994c8586ffcd29d17e06d8
Leo Paggen <lpaggen@gmail.com>
2024-08-03 13:40:55 +0200

    added a function to find and insert attributes of parents in child classes

--- DATAMODELS/jsontoyml.py
+++ DATAMODELS/jsontoyml.py
@@ -134,7 +134,12 @@ def reconstruct_yml(data, out_dir=None):
 
     # process data recursively
     # we need to track many parent-child keys, including some of sublists etc
-    def process(data, parent_key:str = None, children_parent_key:str = None, attribute_parent_key:str = None, default_map:dict = defaults, dn:dict = {}) -> dict:
+    def process(data,
+                parent_key:str = None,
+                grandparent_key:str = None,
+                default_map:dict = defaults,
+                dn:dict = {},
+                dn_key_stack:list = []) -> dict:
         """
 
         Handles mapping of 
@@ -151,27 +156,15 @@ def reconstruct_yml(data, out_dir=None):
 
         changes = []
 
-        yml_tasks = [] # we will also need a list of the nested dictionaries for yml correct format
-
         if isinstance(data, dict):
             keys_to_delete = [] # maintain this list outside any loop, prevents "changed size" errors
 
             for key, value in data.items():
 
-                # handle dn - name of parent in child, grandchild etc etc
-                # if isinstance(value, dict) and "children" in value.keys():
-                #     try:
-                #         dn_key = reverseAliases[classToAnsible[key]]["name"][:-1]
-                #         dn_val = data[key]['attributes']["name"]
-                #         dn[dn_key] = dn_val
-                #     except(KeyError):
-                #         pass
-
                 # all CLASSES are handled, exceptions..
                 # TO DO -> handle exceptions like "changes"
                 # also not quite sure everything we are looking for is exclusively under "children" ...
                 if key == "children":
-                    children_parent_key = parent_key
 
                     # children ALWAYS contains a nested list of nested dictionaries
                     for item in value:
@@ -182,11 +175,10 @@ def reconstruct_yml(data, out_dir=None):
 
                             # handle duplicate mappings FIRST
                             if isduplicate(chld_key):
-                                changes.append((children_parent_key, chld_key, chld_value, 0))
+                                changes.append((parent_key, chld_key, chld_value, 0))
 
                 # all PARAMETERS are processed and mapped if needed
                 elif key == "attributes": # these are all the parameters of the classes
-                    attribute_parent_key = parent_key
 
                     # check if all attributes are default; if so, skip processing entirely
                     # empty dictionaries function will take care of it
@@ -209,35 +201,23 @@ def reconstruct_yml(data, out_dir=None):
                                 print(f"KeyError encountered: {e}")
                                 pass
 
-                        # TYPE 2 -> fix the dn attributes missing from children classes
-                        # if len(dn) > 0:
-                        #     # assuming dn can only be a stack 2 long
-                        #     if len(dn) > 2:
-                        #         dn_list = list(dn.items())
-                        #         dn_list.pop(1)
-                        #         dn = dict(dn_list)
-                        #     for dn_key, dn_val in dn.items():
-                        #         if dn_key == "filter_nam":
-                        #             print(dn_key)
-                        #         changes.append((attribute_parent_key, dn_key, dn_val, 2))
-
                         # TYPE 2 CHANGES -> ATTRIBUTES FIELD REMOVAL, REMOVAL OF DEFAULTS
                         # append all non default params to "changes" - mapping and deletion is done here
                         for attr_key, attr_value in value.items(): # here empty values are also handled, eg if default then don't change
-                            if attr_key not in invisible_args and attr_value not in default_args and not isdefault(attribute_parent_key, attr_key, attr_value, default_map):
+                            if attr_key not in invisible_args and attr_value not in default_args and not isdefault(parent_key, attr_key, attr_value, default_map):
 
                                 # proper to fv_subnet once again?
                                 # new exception found with "ip" -> creates a mask
                                 if attr_key == "ip":
                                     attr_key = "gateway"
-                                    changes.append((attribute_parent_key, 'mask', int(attr_value[-2:]), 2))
+                                    changes.append((parent_key, 'mask', int(attr_value[-2:]), 2))
 
                                 attr_value = attr_value if attr_key != "gateway" else attr_value[:-3]
 
-                                changes.append((attribute_parent_key, attr_key, attr_value, 2)) # appends a tuple
+                                changes.append((parent_key, attr_key, attr_value, 2)) # appends a tuple
 
                         # atm hardcoding "present" into yml, change later if needed
-                        changes.append((attribute_parent_key, 'state', 'present', 2))
+                        changes.append((parent_key, 'state', 'present', 2))
 
                     keys_to_delete.append("attributes") # can append key too
 
@@ -245,7 +225,8 @@ def reconstruct_yml(data, out_dir=None):
                 # therefore we want no exceptions
                 if isinstance(value, (dict, list)):
                     try:
-                        process(value, key, attribute_parent_key, dn)
+                        process(data = value, parent_key = key, grandparent_key = parent_key)
+    
                     except (KeyError) as e:
                         print(f"Error processing key {key}: {e}")
                         pass
@@ -270,7 +251,7 @@ def reconstruct_yml(data, out_dir=None):
         elif isinstance(data, list):
             for item in data:
                 if isinstance(item, (dict, list)):
-                    process(item, parent_key, attribute_parent_key, dn)
+                    process(data = item, parent_key = parent_key, grandparent_key = grandparent_key)
 
         # accessing keys in "data" instead of passing through a parent_key works
         # BECAUSE OF RECURSION, we are handling nested dictionaries as "data" everytime !!!!!!
@@ -319,16 +300,59 @@ def reconstruct_yml(data, out_dir=None):
         else:
             return data # this basically evaluates to false when the structure is None
 
+    # this function gathers all hierarchical parameter names - further used in reconstruct function
+    # returns a dictionary with list values and string keys
+    def get_parent_attributes(data, 
+                          parent_key=None, 
+                          grandparent_key=None, 
+                          great_grandparent_key=None, 
+                          great_great_grandparent_key=None, 
+                          parent_name=None, 
+                          grandparent_name=None, 
+                          great_grandparent_name=None, 
+                          great_great_grandparent_name=None, 
+                          changes=None):
+
+        if changes is None:
+            changes = {}
+
+        if isinstance(data, dict):
+            for key, value in data.items():
+                try:
+                    current_name = reverseAliases[key]["name"][:-1]  # remove mandatory tag
+                except KeyError:
+                    current_name = None
+
+                if parent_key == "children": # need to append all this info for later use - iterate over pairwise, will work just fine
+                    change = [i for i in [parent_name, grandparent_name, great_grandparent_name, great_great_grandparent_name, grandparent_key, great_great_grandparent_key] if i is not None]
+                    changes[key] = change
+
+                # could make it more clear somehow.. 
+                get_parent_attributes(value, key, parent_key, grandparent_key, great_grandparent_key, 
+                                    current_name, parent_name, grandparent_name, great_grandparent_name, changes)
+
+        elif isinstance(data, list):
+            for item in data:
+                get_parent_attributes(item, parent_key, grandparent_key, great_grandparent_key, great_great_grandparent_key, 
+                                    parent_name, grandparent_name, great_grandparent_name, great_great_grandparent_name, changes)
+
+        return changes
+
     # handle reconstruction of yml with recursion
-
-# TO DO -> ADD REMVOVAL OF "fvRs" CLASSES, too hard to handle above due to recursive issues
-
-    def rebuild_yml(data, credentials_file = None, yml_list = [], parent_key = None, grandparent_key = None):
+    # TO DO -> ADD REMVOVAL OF "fvRs" CLASSES, too hard to handle above due to recursive issues
+    def rebuild_yml(data, dn_parent_map = {}, dn_attributes_map = None, credentials_file = None, yml_list = [], parent_key = None, grandparent_key = None):
 
         entry_dict = {}
 
         if isinstance(data, dict):
             for key, value in data.items():
+
+                # get dn attributes
+                try:
+                    parent_attribute = reverseAliases[key]["name"][:-1] # only applies to required params
+                    dn_parent_map[key] = data[key][parent_attribute]
+                except(KeyError):
+                    pass
 
                 nested_dictionary = {}
 
@@ -343,6 +367,7 @@ def reconstruct_yml(data, out_dir=None):
 
                     # set name to something generic
                     key_name = key.split("_")[1] if len(key.split("_")) == 2 else key.split("_")[2]
+
                     if parent_key != None:
                         grandparent_key_name = grandparent_key.split("_")[1] if len(grandparent_key.split("_")) == 2 else grandparent_key.split("_")[2:]
                         sentence = f"add a {key_name.upper()} to the {grandparent_key_name.upper()}"
@@ -354,21 +379,35 @@ def reconstruct_yml(data, out_dir=None):
                         if subkey != "children":
                             # print(key, subkey, subvalue)
                             nested_dictionary[subkey] = subvalue
+
+                    if key in dn_attributes_map:
+
+                        try:
+                            if len(dn_attributes_map[key]) == 2:
+                                nested_dictionary[dn_attributes_map[key][0]] = dn_parent_map[dn_attributes_map[key][1]]
+
+                            elif len(dn_attributes_map[key]) == 4:
+                                nested_dictionary[dn_attributes_map[key][0]] = dn_parent_map[dn_attributes_map[key][2]]
+                                nested_dictionary[dn_attributes_map[key][1]] = dn_parent_map[dn_attributes_map[key][3]]
+
+                        except(KeyError):
+                            pass
+
                     entry_dict["cisco.aci." + key] = nested_dictionary
                     entry_dict["delegate_to"] = "localhost"
 
                     yml_list.append(entry_dict)
 
                 if isinstance(value, dict):
-                    rebuild_yml(value, parent_key = key, grandparent_key = parent_key)
+                    rebuild_yml(value, parent_key = key, grandparent_key = parent_key, dn_attributes_map = dn_attributes_map)
 
                 elif isinstance(value, list):
                     for item in value:
-                        rebuild_yml(item, parent_key = key, grandparent_key = parent_key)
+                        rebuild_yml(item, parent_key = key, grandparent_key = parent_key, dn_attributes_map = dn_attributes_map)
 
         elif isinstance(data, list):
             for item in data:
-                rebuild_yml(item, parent_key = key, grandparent_key = parent_key)
+                rebuild_yml(item, parent_key = key, grandparent_key = parent_key, dn_attributes_map = dn_attributes_map)
 
         return yml_list
 
@@ -378,7 +417,9 @@ def reconstruct_yml(data, out_dir=None):
     # delete the empty dictionaries
     b = remove_empty_dicts(a)
 
-    fin = rebuild_yml(b)
+    c = get_parent_attributes(b)
+
+    fin = rebuild_yml(data = b, dn_attributes_map = c)
 
     return fin
 
